eda848e8c7f199d20bcda68aad20f78a38a234fd
NataliiaMelnyk2025 <nataliia.melnyk@motopp.nl>
2025-08-20 00:00:40 +0200

    delete user =  physically delete records in profile and address + erazing user details, (keeping user_id )+ disable tokens for deleted user account. Updates of USER endpoints

--- crm/settings.py
+++ crm/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     "phonenumber_field",
     "rest_framework",
     "rest_framework_simplejwt",
+    "rest_framework_simplejwt.token_blacklist",
     "corsheaders",
     "django_ses",
     "drf_spectacular",
