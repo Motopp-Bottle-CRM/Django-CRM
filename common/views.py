819e8d5236240bd9df4854a07675f7c07b6167cf
NataliiaMelnyk2025 <nataliia.melnyk@motopp.nl>
2025-09-04 14:54:02 +0200

    fixed - when new user is created his phone and alt phone are saved

--- common/views.py
+++ common/views.py
@@ -142,7 +142,7 @@ class UsersListView(APIView, LimitOffsetPagination):
                     )
                     user.email = user.email
                     user.save()
-                    
+                  
                     # Create profile
                     profile = Profile.objects.create(
                         user=user,
@@ -150,6 +150,8 @@ class UsersListView(APIView, LimitOffsetPagination):
                         role=params.get("role"),
                         address=address_obj,
                         org=request.profile.org,
+                        phone=params.get("phone"),
+                        alternate_phone=params.get("alternate_phone"),
                         is_active=False,  # Profile starts as inactive until password is set
                     )
                     print(f"SUCCESS: Profile created for user: {user.email}, is_active: {profile.is_active}, org: {request.profile.org.id}")
